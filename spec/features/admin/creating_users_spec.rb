325ffa2f800dd5cdec3dcaa65823c384b793e30a
shadowjack <kostyap@hotmail.ru>
2015-10-01 22:55:44 +0300

    Add ability to create new admins for admins

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -22,4 +22,16 @@ feature 'Creating users' do
     click_button 'Create User'
     expect(page).to have_content('User has not been created.')
   end
+
+  scenario 'Crate admin user' do
+    fill_in 'Email', with: 'admin@example.com'
+    fill_in 'Password', with: 'password'
+    check 'Is an admin?'
+    click_button 'Create User'
+    expect(page).to have_content('User has been created.')
+    within('#users') do
+      expect(page).to have_content('admin@example.com (Admin)')
+    end
+  end
+
 end
