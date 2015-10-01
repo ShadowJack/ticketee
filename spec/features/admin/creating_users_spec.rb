4584ca425d71db3b81cc6993410f710411bb9da5
shadowjack <kostyap@hotmail.ru>
2015-10-01 22:46:36 +0300

    Add ability to create new users for admins

---
+++ spec/features/admin/creating_users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'Creating users' do
+  let!(:admin) { FactoryGirl.create(:admin_user) }
+
+  before do
+    sign_in_as!(admin)
+    visit '/'
+    click_link 'Admin'
+    click_link 'Users'
+    click_link 'New User'
+  end
+
+  scenario 'Creating new user' do
+    fill_in 'Email', with: 'newbie@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Create User'
+    expect(page).to have_content('User has been created.')
+  end
+
+  scenario 'Not create user without name or password' do
+    click_button 'Create User'
+    expect(page).to have_content('User has not been created.')
+  end
+end
